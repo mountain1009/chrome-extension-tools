9d183025f5bbe4952804dd9e220698ed6ea73548
大山凌平 <ryohei@ooyamaryoutairanoMacBook-Pro.local>
2023-05-14 12:58:07 +0900

    feat: :+1: first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import BookMarks from './components/BookMarks.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-center text-2xl font-bold">
+      Bookmark
+    </h1>
+    <BookMarks />
+  </div>
+</template>
